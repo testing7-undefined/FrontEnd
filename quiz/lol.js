94ce781aef7da15bff4134f11585c9e312003740
testing7 <131978302+testing7-undefined@users.noreply.github.com>
2023-06-30 13:08:54 +0530

    Update lol.js

--- quiz/lol.js
+++ quiz/lol.js
@@ -1,7 +1,7 @@
 let scr=JSON.parse(localStorage.getItem('scre'));
 document.querySelector('.correct').innerHTML=(scr.crc < 10)?'0'+scr.crc:scr.crc;
 document.querySelector('.incorrect').innerHTML=(scr.inc < 10)?'0'+scr.inc:scr.inc;
-
+document.querySelector('.boo').innerHTML= src.crc;
 function try_again(){
   location.href="index.html";
 }
