e96ee39bf149771effb8703f4bbecd16de71effa
testing7 <131978302+testing7-undefined@users.noreply.github.com>
2023-06-30 13:00:04 +0530

    Delete quiz directory

--- quiz/lol.js
+++
@@ -1,7 +0,0 @@
-let scr=JSON.parse(localStorage.getItem('scre'));
-document.querySelector('.correct').innerHTML=(scr.crc < 10)?'0'+scr.crc:scr.crc;
-document.querySelector('.incorrect').innerHTML=(scr.inc < 10)?'0'+scr.inc:scr.inc;
-
-function try_again(){
-  location.href="index.html";
-}
